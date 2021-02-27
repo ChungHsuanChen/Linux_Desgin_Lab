37e27d05e1fa97e6736c91c95bfeadaaa798545d
ChungHsuan <73817114+ChungHsuanChen@users.noreply.github.com>
2021-02-27 16:58:10 +0800

    Add files via upload

---
+++ clean_queue.c
@@ -0,0 +1,61 @@
+#include<unistd.h>
+#include<sys/time.h>
+#include<sys/syscall.h>
+#include<stdio.h>
+#include<string.h>
+#include<syscall.h>
+#include<sys/types.h>
+
+
+pid_t gettid(void){
+	return syscall(__NR_gettid);
+}
+void main(){
+
+	int random_num1,random_num2;
+	
+	int ttid,count;
+	char str[300];
+	struct timeval t1,t2;
+	gettimeofday(&t1,NULL);
+	ttid=gettid();
+	char f[255];
+
+	sprintf(f,"%d",ttid);
+
+	char fil[]="file_";
+	count=0;
+	FILE *fptr;
+	strcat(fil,f);
+
+	fptr=fopen(fil,"w");
+	while(1){
+		gettimeofday(&t2,NULL);
+		double time1,time2;
+		time1=t1.tv_sec+(t1.tv_sec/1000000.0);
+		time2=t2.tv_sec+(t2.tv_sec/1000000.0);
+		time1=time1-time2;
+		if(time1<0)time1=-time1;
+		if(time1>=300)break;
+
+		
+		random_num1=(rand()%3)+1;
+		random_num2=(rand()%60);
+		sleep(random_num2);
+		sprintf(str,"Wake up all processes in wait queue project2_queue_%d at time %f\n",random_num1, time2);
+		fprintf(fptr,"%s",str);
+
+		syscall(352,random_num1);
+	}
+	fprintf(fptr,"Clean wait wait queue project2_queue_1\n");
+	syscall(352,1);
+	fprintf(fptr,"Clean wait wait queue project2_queue_2\n");
+	syscall(352,2);
+	fprintf(fptr,"Clean wait wait queue project2_queue_3\n");
+	syscall(352,3);
+
+	fclose(fptr);
+
+	printf("process %d completes!\n",ttid);
+
+}
---
+++ enter_wait_queue.c
@@ -0,0 +1,70 @@
+#include<linux/kernel.h>
+#include<linux/wait.h>
+#include<linux/sched.h>
+#include<asm/current.h>
+#include<linux/init.h>
+
+DECLARE_WAIT_QUEUE_HEAD(project2_queue_1);
+DECLARE_WAIT_QUEUE_HEAD(project2_queue_2);
+DECLARE_WAIT_QUEUE_HEAD(project2_queue_3);
+/*
+wait_queue_head_t project2_queue_1;
+wait_queue_head_t project2_queue_2;
+wait_queue_head_t project2_queue_3;*/
+asmlinkage int enter_wait_queue(int x){
+	printk("------------enter wait queue----------");
+	DECLARE_WAITQUEUE(wait,current);
+/*	init_waitqueue_head(&project2_queue_1);
+	init_waitqueue_head(&project2_queue_2);
+	init_waitqueue_head(&project2_queue_3);
+	wait_queue_t wait;
+	init_waitqueue_entry(&wait,current);
+	*/
+
+	if(x==1){
+		printk("------------enter wait queue 1----------");
+		add_wait_queue(&project2_queue_1,&wait);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+		return 1;
+	}	
+	else if(x==2) {
+		printk("------------enter wait queue 2----------");
+		add_wait_queue(&project2_queue_2,&wait);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();		
+		return 2;
+	}else if(x==3){
+		printk("------------enter wait queue 3----------");
+		add_wait_queue(&project2_queue_3,&wait);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+		return 3;
+	}
+	
+
+	return 0;
+}
+
+asmlinkage int clean_wait_queue(int x){
+	printk("------------clean wait queue----------");
+	if(x==1){
+		printk("------------clean wait queue 1----------");
+		wake_up_all(&project2_queue_1);
+		init_waitqueue_head(&project2_queue_1);
+		return 1;
+	}
+	else if(x==2){
+		printk("------------clean wait queue 2----------");
+		wake_up_all(&project2_queue_2);
+		init_waitqueue_head(&project2_queue_2);
+		return 2;
+	}
+	else if(x==3){ 
+		printk("------------clean wait queue 3----------");
+		wake_up_all(&project2_queue_3);
+		init_waitqueue_head(&project2_queue_3);
+		return 3;
+	}
+	return 0;
+}
